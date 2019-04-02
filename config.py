376d8ce11e054faf239b2447334383617593c4a2
E.M. Schimmel <11552344+emschimmel@users.noreply.github.com>
2019-04-02 09:29:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 
 class Config():
-    git_base_branch = "master"
-    gitpath = "gitdump/branches"
+    git_base_branch = ""
+    gitpath = "/Users/e.m.schimmel/Development/bva"
     gitpath_with_master = gitpath+"/"+git_base_branch
-    git_url = "https://github.com/emschimmel/kweb"
-    bad_words = ["var ", "mutable", "throw", "constructor", "new", "if", "= null", "=null", ".*", "= Arraylist", "System.out.println"]
-    warning_words = [".forEach", ".add", ".put", "print", "println"]
+    git_url = "git@github.com:BVA-Holding-BV/bva.git"
+    bad_words = ["var ", "mutable", "throw", "constructor", "new", "if", "= null", "=null", ".*", "= Arraylist", "System.out.println", ";"]
+    warning_words = [".forEach", ".add", ".put", "print", "println", ".get", ".set"]
     compliment_words = [".map", "data class", "enum class", "companion object", "when(", "when (", "?:", "listof", "setof", "mapof", ".let", "private val"]
     allowed_file_extentions = ["kt", "txt", "yml", "md", "pom", "xml", "json", "gradle", "bat", "gitignore", "LICENSE", "gradlew"]
     directories_to_ignore = [".git", ".idea"]
